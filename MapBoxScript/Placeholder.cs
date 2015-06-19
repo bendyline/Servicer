41296c82eb07ab4b40f88ea466ff91742cf32bf6
bendymike <mike@bendyline.com>
2015-06-18 18:41:12 -0700

    Placeholder fix

---
+++ MapBoxScript/Placeholder.cs
@@ -0,0 +1,13 @@
+﻿using BL.Servicer.MapCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BL.Servicer.Maps.Mapbox
+{
+    // this is a placeholder class so that Mapbox has at least one 'real' class in it
+    // otherwise this recompiles every build.
+    public abstract class Placeholder
+    {
+    }
+}
